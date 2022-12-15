c275f33d5f5ba5a210965be984c29ae0526c34d9
Aksh4n <sarshary@gmail.com>
2022-12-15 11:49:20 +0330

    rail????

--- onlinefood/settings.py
+++ onlinefood/settings.py
@@ -37,15 +37,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home' ,
-    'channels',
-    'chat',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home' ,
+    'channels',
+    'chat',
     
 ]
 
